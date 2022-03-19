4987a1b6eed8422c8a7dbae8ba8cf628bb88e676
admin <admin@DESKTOP-CNIHM45>
2022-03-20 00:35:31 +1000

    Add project files.

---
+++ CIPWriter/PLC.cs
@@ -0,0 +1,84 @@
+﻿using CIPStandardLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+// quick and dirty implementation of CIP
+// based on existing python based CIP library from internet
+
+namespace CIPWriter
+{
+    public interface IPLC
+    {
+        void Init(string IPAddress, int Rack = 1, int Slot = 0);
+
+        bool IsConnected { get; }
+        void WriteString(string TagName, string Valueu, int StringSize = 3900);
+        void WriteReal(string TagName, float Value);
+        void WriteInt(string TagName, int Value);
+
+        bool Connect();
+        void Close();
+    }
+
+
+    [ClassInterface(ClassInterfaceType.None)]
+    [ComVisible(true)]
+    public class PLC : IPLC
+    {
+        private CIPConnection _PLC;
+
+        [ComVisible(true)]
+        public bool IsConnected
+        {
+            get { return _PLC == null ? false : _PLC.IsConnected; }
+        }
+
+        [ComVisible(true)]
+        public void Init(string IPAddress, int Rack = 1, int Slot = 0)
+        {
+            _PLC = new CIPConnection(IPAddress, (uint)Rack, (uint)Slot);
+        }
+
+        [ComVisible(true)]
+        public void WriteString(string TagName, string Value, int StringSize = 3900)
+        {
+            // string write is performed to LEN and then followed by DATA{0]
+            _PLC.WriteTag(TagName + ".LEN", Value.Length);
+
+            // then write the whole StringSize, with text and remaining blanks
+            byte[] bytes = Encoding.ASCII.GetBytes(Value);
+            byte[] writeString = new byte[StringSize];
+            Array.Copy(bytes, 0, writeString, 0, bytes.Length);
+            _PLC.WriteTag(TagName + ".DATA[0]", writeString);
+        }
+
+        [ComVisible(true)]
+        public void WriteReal(string TagName, float Value)
+        {
+            _PLC.WriteTag(TagName, Value);
+        }
+
+        [ComVisible(true)]
+        public void WriteInt(string TagName, int Value)
+        {
+            _PLC.WriteTag(TagName, Value);
+        }
+
+        [ComVisible(true)]
+        public bool Connect()
+        {
+            return _PLC.Connect();
+        }
+
+        [ComVisible(true)]
+        public void Close()
+        {
+            _PLC.Disconnect();
+        }
+
+    }
+}
---
+++ Tester/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using CIPWriter;
+
+namespace Tester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object value = new float[3] { 1, 2, 3 };
+            PLC myPLC=new PLC();
+            myPLC.Init("192.168.0.29", 1, 3);
+
+            string tagname = "TotalCount";
+
+            if (myPLC.Connect())
+            {
+                Console.WriteLine("Connected");
+
+                tagname = "TotalCount";
+                myPLC.WriteInt(tagname, 12452);
+
+                tagname = "tag2";
+                myPLC.WriteReal(tagname, 123.44f);
+
+                tagname = "TRK_Global_DictionaryWorkA.Items[9]";
+                string text = "This is test write from C# 123";
+                myPLC.WriteString(tagname, text, 3900);
+
+                Console.ReadLine();
+
+                myPLC.Close();
+            }
+
+
+
+
+
+        }
+
+        
+
+    }
+}
